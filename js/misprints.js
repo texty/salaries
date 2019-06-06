0533860ba126fd84d4eb6ad7a65be0ea486406cb
YevhDr <evgesha.drozdova@gmail.com>
2019-06-06 13:47:04 +0300

    одруківки init

--- js/misprints.js
+++ js/misprints.js
@@ -1,6 +1,8 @@
 /**
  * Created by yevheniia on 05.06.19.
  */
+var jokeWidth = $("#joke")[0].getBoundingClientRect().width / 600;
+
 d3.csv("data/agrep_mistakes_result.csv", function(mistakes) {
 
     mistakes.forEach(function(d) {
@@ -11,13 +13,10 @@ d3.csv("data/agrep_mistakes_result.csv", function(mistakes) {
         return d.mistake === "TRUE" && d.categoryBig != "апостроф"
     });
 
-
     var nested_data = d3.nest()
         .key(function(d) { return d.category; })
         .sortValues(d3.descending)
         .entries(TRUEmistakes);
-
-
 
     var container = d3.select("#MisprintButtons");
 
@@ -27,22 +26,18 @@ d3.csv("data/agrep_mistakes_result.csv", function(mistakes) {
         .append("div");
 
     oneMisprint.append("button")
-            .attr("class", "collapsible")
-            .text(function(d) {
-                return d.key
-            })
-            .on("click", function(ev){
-                let parent = $(this).parent();
-                parent.find(".content").toggleClass("hideElem");
-            });
+         .attr("class", "collapsible")
+         .text(function(d) {
+             return d.key
+         })
+         .on("click", function(ev){
+            let parent = $(this).parent();
+            parent.find(".content").toggleClass("hideElem");
+         });
 
     var pCont = oneMisprint
         .append("div")
         .attr("class", "content hideElem");
-
-
-    var jokeWidth = $("#joke")[0].getBoundingClientRect().width / 600;
-
 
     var pcontent = pCont.selectAll("p")
         .data(function(d) {
@@ -59,20 +54,81 @@ d3.csv("data/agrep_mistakes_result.csv", function(mistakes) {
            });
 
     pcontent.append("span")
-            .style('display', "block")
-            .style("position", "absolute")
-            .style("top", "0")
-            .style("left", "0")
-            .style("z-index", "-1")
-            .style("height", "20px")
-            .style("width", function(k){ return  k.freq * jokeWidth  + "px" })
-            .style("background-color", "#F56380") ;
-
-
-
+        .style('display', "block")
+        .style("position", "absolute")
+        .style("top", "0")
+        .style("left", "0")
+        .style("z-index", "-1")
+        .style("height", "20px")
+        .style("width", function(k){ return  k.freq * jokeWidth  + "px" })
+        .style("background-color", "#F56380") ;
 
     pCont.selectAll(".misprintCases").sort(function(a,b){
+         return d3.ascending(a.freq, b.freq)
+    });
+
+
+
+    /*----------------------------------------*/
+    var ap_TRUEmistakes = mistakes.filter(function (d){
+        return d.mistake === "TRUE" && d.categoryBig === "апостроф"
+    });
+
+    var ap_nested_data = d3.nest()
+        .key(function(d) { return d.category; })
+        .sortValues(d3.descending)
+        .entries(ap_TRUEmistakes);
+
+    var ap_container = d3.select("#ap_MisprintButtons");
+
+
+    var ap_oneMisprint = ap_container.selectAll("div")
+        .data(ap_nested_data)
+        .enter()
+        .append("div");
+
+
+    ap_oneMisprint.append("button")
+        .attr("class", "collapsible")
+        .text(function(d) {
+            return d.key
+        })
+        .on("click", function(ev){
+            let parent = $(this).parent();
+            parent.find(".content").toggleClass("hideElem");
+        });
+
+    var ap_pCont = ap_oneMisprint
+        .append("div")
+        .attr("class", "content hideElem");
+
+    var ap_pcontent = ap_pCont.selectAll("p")
+        .data(function(d) {
+            return d.values
+        })
+        .enter()
+        .append("p");
+
+    ap_pcontent
+        .attr("class", "misprintCases")
+        .style("position", "relative")
+        .text(function(k){
+            return k.case + " / " + k.freq
+        });
+
+    ap_pcontent.append("span")
+        .style('display', "block")
+        .style("position", "absolute")
+        .style("top", "0")
+        .style("left", "0")
+        .style("z-index", "-1")
+        .style("height", "20px")
+        .style("width", function(k){ return  k.freq * jokeWidth  + "px" })
+        .style("background-color", "#F56380") ;
+
+    ap_pCont.selectAll(".misprintCases").sort(function(a,b){
         return d3.ascending(a.freq, b.freq)
     })
 
-    });
+
+});
